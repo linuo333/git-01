9abf2c45f0915ca2e4a58ffe95b6fae5301e4279
yanruo <123@qq.com>
2022-05-10 20:32:11 +0800

    6 commit

--- src/main/java/com/linuo/Main.java
+++ src/main/java/com/linuo/Main.java
@@ -3,5 +3,7 @@ package com.linuo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("my name is linuo");
+
+        System.out.println(1+2222);
     }
 }
